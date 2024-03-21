a230b467808cfcc011db1818c7fba841c4369b7d
Steve Pasche <steve.pasche@heig-vd.com>
2024-03-21 15:39:57 +0100

    added all the houses and there specialities

--- src/js/house.js
+++ src/js/house.js
@@ -1,87 +1,111 @@
 const section = document.querySelector("#house");
 import { gryffindor } from "./gryffindor";
+import  { slytherin } from "./slytherin";
+import { ravenclaw } from "./ravenclaw";
+import { hufflepuff } from "./hufflepuff";
 
-export function houseBasics(houseName){
-    switch (houseName) {
-        case "gryffindor" : 
-            houseName = gryffindor;
-            break;
-        default : 
-            break;
-    }
 
-    const div = document.createElement("div");
-    div.classList.add("houseBasics");
+export function houseBasics(houseName) {
+  switch (houseName) {
+    case "gryffindor":
+      houseName = gryffindor;
+      break;
+    case "ravenclaw":
+      houseName = ravenclaw;
+      break;
+    case "slytherin":
+      houseName = slytherin;
+      break;
+    case "hufflepuf":
+      houseName = hufflepuff;
+      break;
+    default:
+      break;
+  }
 
-    const title = document.createElement("h1");
-    title.textContent = houseName.quote;
-    const basic = document.createElement("div");
-    basic.classList.add("basics");
-    const animal = document.createElement("h2");
-    animal.textContent = "Animal";
-    const pAnimal = document.createElement("p");
-    pAnimal.textContent = houseName.animal;
-    
-    const elem = document.createElement("h2");
-    elem.textContent = "Element";
-    const pElem = document.createElement("p");
-    pElem.textContent = houseName.element;
-    
-    const color = document.createElement("h2");
-    color.textContent = "Colors";
-    const pColor = document.createElement("p");
-    pColor.textContent = houseName.colors;
+  const div = document.createElement("div");
+  div.classList.add("houseBasics");
 
-    const founder = document.createElement("h2");
-    founder.textContent = "Founder";
-    const pFounder = document.createElement("p");
-    pFounder.textContent = houseName.founder;
-    
-    const trait = document.createElement("h2");
-    trait.textContent = "Traits";
-    const pTrait = document.createElement("p");
-    pTrait.textContent = houseName.traits;
+  const title = document.createElement("h1");
+  title.textContent = houseName.quote;
+  const basic = document.createElement("div");
+  basic.classList.add("basics");
+  const animal = document.createElement("h2");
+  animal.textContent = "Animal";
+  const pAnimal = document.createElement("p");
+  pAnimal.textContent = houseName.animal;
 
-    basic.append(animal, pAnimal, elem, pElem, color, pColor, founder, pFounder, trait, pTrait);
+  const elem = document.createElement("h2");
+  elem.textContent = "Element";
+  const pElem = document.createElement("p");
+  pElem.textContent = houseName.element;
 
-    const char = document.createElement("div");
-    char.classList.add("characters");
-    //personnage principal 1
-    const main1 = document.createElement("div");
-    main1.classList.add("mainCharacter");
-    const name1 = document.createElement("h2");
-    name1.textContent = houseName.main1.name;
-    const descr1 = document.createElement("p");
-    descr1.textContent = houseName.main1.descr;
-    const img1 = document.createElement("IMG");
-    img1.src = houseName.main1.img;
-    main1.append(img1, name1, descr1);
+  const color = document.createElement("h2");
+  color.textContent = "Colors";
+  const pColor = document.createElement("p");
+  pColor.textContent = houseName.colors;
 
-    //personnage principal 2
-    const main2 = document.createElement("div");
-    main2.classList.add("mainCharacter");
-    const name2 = document.createElement("h2");
-    name2.textContent = houseName.main2.name;
-    const descr2 = document.createElement("p");
-    descr2.textContent = houseName.main2.descr;
-    const img2 = document.createElement("IMG");
-    img2.src = houseName.main2.img;
-    main2.append(img2, name2, descr2);
+  const founder = document.createElement("h2");
+  founder.textContent = "Founder";
+  const pFounder = document.createElement("p");
+  pFounder.textContent = houseName.founder;
 
-    //personnage principal 3
-    const main3 = document.createElement("div");
-    main3.classList.add("mainCharacter");
-    const name3 = document.createElement("h2");
-    name3.textContent = houseName.main3.name;
-    const descr3 = document.createElement("p");
-    descr3.textContent = houseName.main3.descr;
-    const img3 = document.createElement("IMG");
-    img3.src = houseName.main3.img;
-    main3.append(img3, name3, descr3);
+  const trait = document.createElement("h2");
+  trait.textContent = "Traits";
+  const pTrait = document.createElement("p");
+  pTrait.textContent = houseName.traits;
 
-    char.append(main1, main2, main3);
+  basic.append(
+    animal,
+    pAnimal,
+    elem,
+    pElem,
+    color,
+    pColor,
+    founder,
+    pFounder,
+    trait,
+    pTrait
+  );
 
-    //ajouter au dom
-    div.append(title, basic, char);
-    section.append(div);
+  const char = document.createElement("div");
+  char.classList.add("characters");
+  //personnage principal 1
+  const main1 = document.createElement("div");
+  main1.classList.add("mainCharacter");
+  const name1 = document.createElement("h2");
+  name1.textContent = houseName.main1.name;
+  const descr1 = document.createElement("p");
+  descr1.textContent = houseName.main1.descr;
+  const img1 = document.createElement("IMG");
+  img1.src = houseName.main1.img;
+  main1.append(img1, name1, descr1);
+
+  //personnage principal 2
+  const main2 = document.createElement("div");
+  main2.classList.add("mainCharacter");
+  const name2 = document.createElement("h2");
+  name2.textContent = houseName.main2.name;
+  const descr2 = document.createElement("p");
+  descr2.textContent = houseName.main2.descr;
+  const img2 = document.createElement("IMG");
+  img2.src = houseName.main2.img;
+  main2.append(img2, name2, descr2);
+
+  //personnage principal 3
+  const main3 = document.createElement("div");
+  main3.classList.add("mainCharacter");
+  const name3 = document.createElement("h2");
+  name3.textContent = houseName.main3.name;
+  const descr3 = document.createElement("p");
+  descr3.textContent = houseName.main3.descr;
+  const img3 = document.createElement("IMG");
+  img3.src = houseName.main3.img;
+  main3.append(img3, name3, descr3);
+
+  char.append(main1, main2, main3);
+
+  //ajouter au dom
+  div.append(title, basic, char);
+  section.append(div);
 }
---
+++ src/js/slytherin.js
@@ -0,0 +1,38 @@
+// @rollup/plugin-dsv
+import characters from "../../data/HPCharactersData.csv";
+
+let HPDataRow = characters.find((e) => e.Name === "Severus Snape");
+let description1 = HPDataRow.Descr;
+let HPDataRow2 = characters.find((e) => e.Name === "Draco Malfoy");
+let description2 = HPDataRow2.Descr;
+let HPDataRow3 = characters.find((e) => e.Name === "Bellatrix Lestrange");
+let description3 = HPDataRow3.Descr;
+
+const slytherin = {
+    "quote" : "Slytherin will help you on your way to greatness!",
+    "animal" : "Snake",
+    "element" : "Water",
+    "colors" : "Green and silver",
+    "traits" : "Ambition, Cunning, Leadership, Resourcefulness, Self-Preservation, Determination",
+    "founder" : "Salazar Slytherin",
+    "main1" : {
+        "name" : "Severus Snape",
+        "descr" : description1,
+        "img" : "../../assets/img/characters/severus-snape.png",
+    },
+    "main2" : {
+        "name" : "Draco Malfoy",
+        "descr" : description2,
+        "img" : "../../assets/img/characters/draco-malfoy.png",
+    },
+    "main3" : {
+        "name" : "Bellatrix Lestrange",
+        "descr" : description3,
+        "img" : "../../assets/img/characters/bellatrix-lestrange.png",
+    },
+};
+
+
+const personnesSlytherin = characters.filter((e) => e.School.match("slytherin"));
+
+export {slytherin, personnesSlytherin}
