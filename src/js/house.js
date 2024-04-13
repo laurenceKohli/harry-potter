5594166d8e81703e0738021e7b74dfcf4b1dc08f
Laurence Kohli <laurence.kohli@hes-so.ch>
2024-04-13 19:31:01 +0200

    srcoll personnages approximatif

--- src/js/house.js
+++ src/js/house.js
@@ -1,12 +1,14 @@
-const section = document.querySelector("#house");
+const mainSection = document.querySelector("#house");
 import { gryffindor } from "./gryffindor";
 import  { slytherin } from "./slytherin";
 import { ravenclaw } from "./ravenclaw";
 import { hufflepuff } from "./hufflepuff";
+import { Scene } from "scrollmagic";
 
+let houseName = "";
 
-export function houseBasics(houseName) {
-  switch (houseName) {
+export function houseBasics(name) {
+  switch (name) {
     case "gryffindor":
       houseName = gryffindor;
       break;
@@ -26,12 +28,27 @@ export function houseBasics(houseName) {
   const div = document.createElement("div");
   div.classList.add("houseBasics");
 
+  div.append(
+    displayBasics(),
+    displayMainCharacters()
+  );
+  //ajouter au dom
+  mainSection.append(div);
+}
+
+function displayBasics(){
+  const section = document.createElement("section");
+  section.setAttribute('id', 'basics');
+
   const title = document.createElement("h1");
   title.textContent = houseName.quote;
+
   const basic = document.createElement("div");
   basic.classList.add("basics");
+
   const animal = document.createElement("h2");
   animal.textContent = "Animal";
+
   const pAnimal = document.createElement("p");
   pAnimal.textContent = houseName.animal;
 
@@ -52,6 +69,7 @@ export function houseBasics(houseName) {
 
   const trait = document.createElement("h2");
   trait.textContent = "Traits";
+
   const pTrait = document.createElement("p");
   pTrait.textContent = houseName.traits;
 
@@ -68,11 +86,24 @@ export function houseBasics(houseName) {
     pTrait
   );
 
+  section.append(title, basic);
+  return section;
+}
+
+function displayMainCharacters(){
+  const section = document.createElement("section");
+  section.setAttribute('id', 'mainCharacters');
+
+  const title = document.createElement("h2");
+  title.textContent = "The 3 mains characters"
+
   const char = document.createElement("div");
   char.classList.add("characters");
+
   //personnage principal 1
   const main1 = document.createElement("div");
   main1.classList.add("mainCharacter");
+  main1.setAttribute('id', 'first');
   const name1 = document.createElement("h2");
   name1.textContent = houseName.main1.name;
   const descr1 = document.createElement("p");
@@ -84,6 +115,7 @@ export function houseBasics(houseName) {
   //personnage principal 2
   const main2 = document.createElement("div");
   main2.classList.add("mainCharacter");
+  main2.setAttribute('id', 'second');
   const name2 = document.createElement("h2");
   name2.textContent = houseName.main2.name;
   const descr2 = document.createElement("p");
@@ -95,6 +127,7 @@ export function houseBasics(houseName) {
   //personnage principal 3
   const main3 = document.createElement("div");
   main3.classList.add("mainCharacter");
+  main3.setAttribute('id', 'third');
   const name3 = document.createElement("h2");
   name3.textContent = houseName.main3.name;
   const descr3 = document.createElement("p");
@@ -104,8 +137,36 @@ export function houseBasics(houseName) {
   main3.append(img3, name3, descr3);
 
   char.append(main1, main2, main3);
+  section.append(title, char);
 
-  //ajouter au dom
-  div.append(title, basic, char);
-  section.append(div);
+  return section;
 }
+
+window.addEventListener('scroll', function() {
+  const section = document.querySelector('#mainCharacters');
+  const sectionTop = section.getBoundingClientRect().top;
+
+  if(sectionTop < 0 && sectionTop > -1){
+    document.querySelector(".characters").classList.add('inScroll');
+    section.classList.add('fixed');
+    document.querySelector('#mainCharacters > h2').classList.add('fixed');
+    document.querySelector('#first').classList.add('fixed');
+     //console.log(document.querySelector("#second").scrollTop )
+     let topTo = 1002;
+     let top2To = 1002;
+     document.querySelector(".characters").addEventListener('scroll', function(e){
+      topTo -= 10;
+      if(topTo > 0){
+        document.querySelector("#second").style.top = topTo+"px";
+      } else {
+        document.querySelector('#second').classList.add('fixed');
+        top2To -= 10;
+        if(top2To > 0){
+          document.querySelector("#third").style.top = top2To+"px";
+        } else {
+          document.querySelector('#third').classList.add('fixed');
+        }
+      }    
+     })
+  }
+});
