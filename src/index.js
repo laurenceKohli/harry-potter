5b420a2c4d1f7c95c9dd483ab67358bd21d47450
Laurence Kohli <laurence.kohli@hes-so.ch>
2024-04-19 15:03:52 +0200

    question 1 à la fois

--- src/index.js
+++ src/index.js
@@ -5,33 +5,9 @@
 
 import { houseBasics } from "./js/house";
 import { displayFilm } from "./js/film";
+import { questionsUser, displayQuestion } from "./js/questions";
 
-houseBasics("gryffindor");
+houseBasics("ravenclaw");
 displayFilm(1,"gryffindor");
 
-
-//questions
-import { questionsUser } from "./js/questions";
- 
-function displayQuestions() {
-  const section = document.getElementById("questions");
-  section.classList.remove("hide"); // Affiche la section si elle était cachée
- 
-  Object.values(questionsUser).forEach((item, index) => {
-    const questionElement = document.createElement("div");
-    questionElement.innerHTML = `<h3>${item.question}</h3>`;
- 
-    const answersList = document.createElement("ul");
-    Object.entries(item.réponse).forEach(([answer, value]) => {
-      const answerItem = document.createElement("li");
-      answerItem.textContent = answer;
-      answersList.appendChild(answerItem);
-    });
- 
-    questionElement.appendChild(answersList);
-    section.appendChild(questionElement);
-  });
-}
- 
-// Appel de la fonction pour afficher les questions et réponses
-displayQuestions();
+displayQuestion(1);
