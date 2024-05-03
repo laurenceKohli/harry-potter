30395ec5576e7cfe6227f5613ee5c5ca326704a9
Laurence Kohli <laurence.kohli@hes-so.ch>
2024-05-03 16:13:50 +0200

    Hover sans cartes manquantes

--- src/js/graphHover.js
+++ src/js/graphHover.js
@@ -36,37 +36,88 @@ function graphOver() {
  
 
 
-function showCard(e) {
-    const card = document.querySelector(".hp-quiz-card");
-    const hover = document.querySelector(".hp-quiz-card h1");
-    const currentName = e.target.classList.value;
+// function showCard(e) {
+//     e.stopPropagation();
+//     const card = document.querySelector(".hp-quiz-card");
+//     const hover = document.querySelector(".hp-quiz-card h1");
+//     console.log(e.target)
+//     const currentName = e.target.id;
     
 
-    if (!(hover.textContent = ((currentName) == ""))) {
-        const dataCaracter = filmCaracters.find((e) => e.Name == currentName)
-        if ( currentName == "totalScreen" ){
-            //je ne veux afficher que ceux qui ont une carte à leur nom (totalScreen n'est pas dans Harry Potter ;-) )
-        }else if (card) {
-            card.classList.remove("hide");
-            card.style.position = "fixed";
-            card.style.left = "50%";
-            card.style.top = "50%";
-            card.style.transform = "translate(-50%, -50%)";
-            card.style.zIndex = "1000";
-            card.querySelector('.hp-quiz-card .sidebar').style.backgroundColor = `var(--${houseNow}-primary-card-color)`;
-            card.querySelector('.hp-quiz-card .menu-item.active ').style.backgroundColor = `var(--${houseNow}-secondary-card-color)`;
-            card.querySelector('.hp-quiz-card .card').style.backgroundColor = `var(--${houseNow}-background-card-color)`;
-            card.querySelector('.hp-quiz-card .house ').style.color = `var(--${houseNow}-secondary-card-color)`;
-            hover.textContent = (e.target.classList.value == "")? "NaN" : e.target.classList.value;
-            document.querySelector('.content p:nth-child(2)').textContent = dataCaracter.Blood;
-            document.querySelector('.content p:nth-child(3)').textContent = dataCaracter.Gender;
-            document.querySelector('.content p:nth-child(4)').textContent = dataCaracter.Descr;
-            document.querySelector('.house').textContent= houseNow;
-            console.log(houseNow);
+//     if (!(hover.textContent = ((currentName) == ""))) {
+//         const dataCaracter = filmCaracters.find((e) => e.Name == currentName)
+//         if ( currentName == "totalScreen" ){
+//             //je ne veux afficher que ceux qui ont une carte à leur nom (totalScreen n'est pas dans Harry Potter ;-) )
+//         }else {
+//             card.style.position = "fixed";
+//             card.style.left = "50%";
+//             card.style.top = "50%";
+//             card.style.transform = "translate(-50%, -50%)";
+//             card.style.zIndex = "1000";
+//             card.querySelector('.hp-quiz-card .sidebar').style.backgroundColor = `var(--${houseNow}-primary-card-color)`;
+//             card.querySelector('.hp-quiz-card .menu-item.active ').style.backgroundColor = `var(--${houseNow}-secondary-card-color)`;
+//             card.querySelector('.hp-quiz-card .card').style.backgroundColor = `var(--${houseNow}-background-card-color)`;
+//             card.querySelector('.hp-quiz-card .house ').style.color = `var(--${houseNow}-secondary-card-color)`;
+//             hover.textContent = (currentName == "")? "NaN" : currentName;
+//             // document.querySelector('.content p:nth-child(2)').textContent = dataCaracter.Blood;
+//             // document.querySelector('.content p:nth-child(3)').textContent = dataCaracter.Gender;
+//             // document.querySelector('.content p:nth-child(4)').textContent = dataCaracter.Descr;
+//             // document.querySelector('.house').textContent= houseNow;
+//             //console.log(houseNow);
+//             card.classList.remove("hide");
 
-        }
+
+//         }
+//     }
+// }
+
+function showCard(e) {
+    e.stopPropagation();
+    const card = document.querySelector(".hp-quiz-card");
+    const hover = document.querySelector(".hp-quiz-card h1");
+    const currentName = e.target.id;
+
+    // Correction ici : utiliser l'opérateur de comparaison pour vérifier si currentName est vide
+    if (currentName === "") {
+        return; // Si currentName est vide, sortir de la fonction
+    }
+
+    let dataCaracter ="";
+    if (currentName == "Professor Severus Snape") {
+        dataCaracter = filmCaracters.find((e) => e.Name == "Severus Snape");
+    } else if (currentName == "Professor Filius Flitwick"){
+        dataCaracter = filmCaracters.find((e) => e.Name == "Filius Flitwick");
+    } else if (currentName == "Professor Gilderoy Lockhart"){
+        dataCaracter = filmCaracters.find((e) => e.Name == "Gilderoy Lockhart");
+    } else if(currentName == "Professor Pomona Sprout"){
+        dataCaracter = filmCaracters.find((e) => e.Name == "Pomona Sprout");
+    }
+    else {
+        dataCaracter = filmCaracters.find((e) => e.Name == currentName);
+    }
+    // Gestion spécifique pour "totalScreen"
+    if (currentName === "totalScreen") {
+        // Ne rien faire pour "totalScreen"
+        return;
+    } else {
+        card.style.position = "fixed";
+        card.style.left = "50%";
+        card.style.top = "50%";
+        card.style.transform = "translate(-50%, -50%)";
+        card.style.zIndex = "1000";
+        card.querySelector('.hp-quiz-card .sidebar').style.backgroundColor = `var(--${houseNow}-primary-card-color)`;
+        card.querySelector('.hp-quiz-card .menu-item.active ').style.backgroundColor = `var(--${houseNow}-secondary-card-color)`;
+        card.querySelector('.hp-quiz-card .card').style.backgroundColor = `var(--${houseNow}-background-card-color)`;
+        card.querySelector('.hp-quiz-card .house ').style.color = `var(--${houseNow}-secondary-card-color)`;
+        hover.textContent = currentName;
+        document.querySelector('.content p:nth-child(2)').textContent = dataCaracter.Blood;
+        document.querySelector('.content p:nth-child(3)').textContent = dataCaracter.Gender;
+        document.querySelector('.content p:nth-child(4)').textContent = dataCaracter.Descr;
+        document.querySelector('.house').textContent= houseNow;
+        card.classList.remove("hide");
     }
 }
+
  
 function hideCard() {
     const card = document.querySelector(".hp-quiz-card");
@@ -79,7 +130,12 @@ function timesOfFilm(filmTitle) {
     let filmRows = filmTime.filter((e) => e.Movie === filmTitle);
     let timeRows = [];
     peopleNow.forEach(personne => {
-        const line = filmRows.filter((e) => e.Character.match(personne.Name));
+        let line= ""
+        if (personne.Name == "Professor Rubeus Hagrid") {
+           line = filmRows.filter((e) => e.Character.match("Rubeus Hagrid"));
+        }else{
+            line = filmRows.filter((e) => e.Character.match(personne.Name));
+        }
         if (line.length > 0) {
             if (!isNaN(tempsEnMilliseconds(line[0].ScreenTime))) {
                 timeRows.push(line[0])
@@ -97,7 +153,12 @@ function averagePerPerson(){
         const film = titleFilm(index);
         const times = timesOfFilm(film[0]);
         times.forEach(personne => {
-            const alreadyIn = tempsTotal.filter((e) => e.Character.match(personne.Character))
+            let alreadyIn = "";
+            if (personne.Character == "Professor Rubeus Hagrid") {
+                alreadyIn = tempsTotal.filter((e) => e.Character.match("Rubeus Hagrid"));
+             }else{
+                alreadyIn = tempsTotal.filter((e) => e.Character.match(personne.Character))
+            }
             if (alreadyIn.length > 0) {
                 alreadyIn[0].ScreenTime += tempsEnMilliseconds(personne.ScreenTime);
             } else {
@@ -105,6 +166,7 @@ function averagePerPerson(){
                     "Character": personne.Character, 
                     "ScreenTime": tempsEnMilliseconds(personne.ScreenTime)
                 }
+                console.log(personne.Character)
                 tempsTotal.push(send)
             }
         })
@@ -134,7 +196,7 @@ function creeBarCharVer(donnees){
             .attr("y", d => yScale(d.ScreenTime))
             .attr("width", 30)
             .attr("height", (d, i) => height - yScale(d.ScreenTime))
-            .attr("class", d => d.Character)
+            .attr("id", d => d.Character)
             .attr("fill", (d, i) => (i%2 == 0) ? `var(--${houseNow}-secondary-card-color)` : `var(--${houseNow}-secondary-color)`));
             // TODO a remplacer par ${houseNow} à la place du nom de la maison
  
